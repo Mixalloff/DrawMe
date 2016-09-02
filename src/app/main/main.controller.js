60a6248fa1edf1805fd1dc1a27b8bdbe20679b41
Mikhail Mikhalev <Mikhail Mikhalev>
2016-09-02 20:20:31 +0300

    Deleted Auth pages, added register dialog

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,12 +1,50 @@
 (function() {
     angular
         .module('app.main')
-        .controller('MainController', function($mdSidenav) {
+        .controller('MainController', function($mdSidenav, $mdDialog) {
             var vm = this;
             vm.toogleNav = toogleNav;
+            vm.openRegisterDialog = openRegisterDialog;
             
             function toogleNav(navID) {
                 $mdSidenav(navID).toggle();
+            }
+
+            function openRegisterDialog(ev) {
+                $mdDialog.show({
+                    controller: RegisterDialogController,
+                    controllerAs: 'register',
+                    templateUrl: 'app/components/auth/views/registerDialog.html',
+                    parent: angular.element(document.body),
+                    targetEvent: ev,
+                    clickOutsideToClose: false,
+                })
+                .then(function(newUserData) {
+                    console.dir(newUserData);
+                }, function() {
+                    // закрыто диалоговое окно
+                });
+            }
+
+            function RegisterDialogController($mdDialog) {
+                var vm = this;
+                vm.ok = ok;
+                vm.cancel = cancel;
+                vm.roles = getRegisterRoles();
+
+                function ok(answer) {
+                    $mdDialog.hide(answer);
+                };
+                function cancel() {
+                    $mdDialog.cancel();
+                };
+
+                function getRegisterRoles() {
+                    return [
+                        { name: 'Художник', type: 'drawer', id: 1 },
+                        { name: 'Заказчик', type: 'customer', id: 2 }
+                    ]
+                }
             }
         });
 })();
